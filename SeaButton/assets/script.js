adea8d8e971d9c4c0de2adc127979c11b7009542
AIias-Dev <taruno.bcl@gmail.com>
2023-11-17 10:24:59 +0100

    Update script.js

--- SeaButton/assets/script.js
+++ SeaButton/assets/script.js
@@ -1,47 +1,50 @@
 let paragraphe = document.querySelector("#paragraphe");
-let blobButton = document.querySelector("#blob-button");
+let seaButton = document.querySelector("#sea-button");
 let arrow = document.querySelector(".gg-arrow-right");
-let blob1 = document.querySelector("#blob1");
-let blob2 = document.querySelector("#blob2");
-let blob3 = document.querySelector("#blob3");
-let blob4 = document.querySelector("#blob4");
+let sea1 = document.querySelector("#sea1");
+let sea2 = document.querySelector("#sea2");
+let sea3 = document.querySelector("#sea3");
+let sea4 = document.querySelector("#sea4");
 
-blobButton.addEventListener("mouseover", function () {
+seaButton.addEventListener("mouseover", function () {
   paragraphe.style.color = "white";
   paragraphe.style.transition = "color 0.5s ease";
 
-  blob1.style.top = "-25%";
-  blob4.style.top = "-25%";
-  blob3.style.top = "-25%";
-  blob2.style.top = "-25%";
-  blob1.style.transition = "top 0.2s ease";
-  blob2.style.transition = "top 0.4s ease";
-  blob3.style.transition = "top 0.6s ease";
-  blob4.style.transition = "top 0.8s ease";
+  sea1.style.top = "-25%";
+  sea2.style.top = "-25%";
+  sea3.style.top = "-25%";
+  sea4.style.top = "-25%";
+
+  sea1.style.transition = "top 0.2s ease";
+  sea2.style.transition = "top 0.4s ease";
+  sea3.style.transition = "top 0.6s ease";
+  sea4.style.transition = "top 0.8s ease";
 
   paragraphe.addEventListener("transitionend", function () {
     const charCount = paragraphe.innerText.length;
     
-    let value = 30 - charCount;
+    let value = 28 - charCount;
 
     paragraphe.style.transform = `translateX(-${value}%)`;
     paragraphe.style.transition = 'transform 0.4s ease';
     arrow.style.opacity = "1";
-    arrow.style.bottom = "8%";
+    arrow.style.bottom = "0%";
   });
 });
 
-blobButton.addEventListener("mouseleave", function () {
+seaButton.addEventListener("mouseleave", function () {
   paragraphe.style.color = "#0505a9";
   paragraphe.style.transition = "color 0.5s ease";
-  blob1.style.top = "100%";
-  blob4.style.top = "100%";
-  blob3.style.top = "100%";
-  blob2.style.top = "100%";
-  blob1.style.transition = "top 0.2s ease";
-  blob2.style.transition = "top 0.4s ease";
-  blob3.style.transition = "top 0.6s ease";
-  blob4.style.transition = "top 0.8s ease";
+  
+  sea1.style.top = "100%";
+  sea2.style.top = "100%";
+  sea3.style.top = "100%";
+  sea4.style.top = "100%";
+  
+  sea1.style.transition = "top 0.2s ease";
+  sea2.style.transition = "top 0.4s ease";
+  sea3.style.transition = "top 0.6s ease";
+  sea4.style.transition = "top 0.8s ease";
 
   paragraphe.addEventListener("transitionend", function () {
 
